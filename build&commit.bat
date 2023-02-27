c62fabcba1ee2095881379707be680cc0b009538
cyber-ikegami <ikegami@cyberbeing.tech>
2023-02-27 13:23:11 +0900

    commit

--- build&commit.bat
+++ build&commit.bat
@@ -16,3 +16,9 @@ xcopy /e /y C:\app\workspace\git\camJTableTool\front_tool\build C:\app\workspace
 xcopy /e /y C:\app\workspace\git\csv_converter\build C:\app\workspace\git\ikegamiTool\csvConverter
 
 xcopy /e /y C:\app\workspace\git\projectTemplate_react\build C:\app\workspace\git\ikegamiTool\projectTemplate
+
+rem コミット----------------------------------------------
+cd C:\app\workspace\git\ikegamiTool
+git add .
+git commit -m "commit"
+git push origin master
--- commit.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-rem コミット----------------------------------------------
-cd C:\app\workspace\git\ikegamiTool
-git add .
-git commit -m "commit"
-git push origin master
